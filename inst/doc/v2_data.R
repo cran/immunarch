a1786016777722c8435c0b5f25cdca1ddcfdff51
Vadim I. Nazarov <support@immunomind.io>
2024-03-18 19:10:06 +0000

    version 0.9.1

--- inst/doc/v2_data.R
+++ inst/doc/v2_data.R
@@ -12,19 +12,19 @@ library(immunarch)
 # immdata = load_test_data()
 data(immdata)
 
-## ---- eval=F------------------------------------------------------------------
-#  #path argument is a path to the folder with your file or files including the metadata file.
+## ----eval=F-------------------------------------------------------------------
+#  # path argument is a path to the folder with your file or files including the metadata file.
 #  immdata <- repLoad(path)
 
-## ---- eval=F------------------------------------------------------------------
-#  immdata <- repLoad('example')
+## ----eval=F-------------------------------------------------------------------
+#  immdata <- repLoad("example")
 
-## ---- eval=F------------------------------------------------------------------
-#  #path to the folder with example data
-#  file_path = paste0(system.file(package="immunarch"), "/extdata/io/")
+## ----eval=F-------------------------------------------------------------------
+#  # path to the folder with example data
+#  file_path <- paste0(system.file(package = "immunarch"), "/extdata/io/")
 #  immdata <- repLoad(file_path)
 
-## ---- eval=F------------------------------------------------------------------
+## ----eval=F-------------------------------------------------------------------
 #  # For instance you have a following structure in your folder:
 #  # >_ ls
 #  # immunoseq1.txt
@@ -32,9 +32,9 @@ data(immdata)
 #  # immunoseq3.txt
 #  # metadata.txt
 
-## ---- eval=F------------------------------------------------------------------
+## ----eval=F-------------------------------------------------------------------
 #  # To load the whole folder with every file in it type:
-#  file_path = paste0(system.file(package="immunarch"), "/extdata/io/")
+#  file_path <- paste0(system.file(package = "immunarch"), "/extdata/io/")
 #  immdata <- repLoad(file_path)
 #  print(names(immdata))
 #  
@@ -50,35 +50,34 @@ data(immdata)
 #  # > immdata <- repLoad("path/to/your/folder/")
 #  # > names(immdata)
 #  # [1] "data" "meta"
-#  
 
 ## -----------------------------------------------------------------------------
 as_tibble(data.frame(Sample = c("immunoseq1", "immunoseq2", "immunoseq3"), stringsAsFactors = F))
 
-## ---- eval=F------------------------------------------------------------------
+## ----eval=F-------------------------------------------------------------------
 #  # Your list of repertoires in immunarch's format
 #  DATA
 #  # Metadata data frame
 #  META
 #  
 #  # Create a temporary directory
-#  dbdir = tempdir()
+#  dbdir <- tempdir()
 #  
 #  # Create a DBI connection to MonetDB in the temporary directory.
-#  con = DBI::dbConnect(MonetDBLite::MonetDBLite(), embedded = dbdir)
+#  con <- DBI::dbConnect(MonetDBLite::MonetDBLite(), embedded = dbdir)
 #  
 #  # Write each repertoire to MonetDB. Each table has corresponding name from the DATA
-#  for (i in 1:length(DATA)) {
-#    DBI::dbWriteTable(con, names(DATA)[i], DATA[[i]], overwrite=TRUE)
+#  for (i in seq_along(DATA)) {
+#    DBI::dbWriteTable(con, names(DATA)[i], DATA[[i]], overwrite = TRUE)
 #  }
 #  
 #  # Create a source in the temporary directory with MonetDB
-#  ms = MonetDBLite::src_monetdblite(dbdir = dbdir)
-#  res_db = list()
+#  ms <- MonetDBLite::src_monetdblite(dbdir = dbdir)
+#  res_db <- list()
 #  
 #  # Load the data from MonetDB to dplyr tables
-#  for (i in 1:length(DATA)) {
-#    res_db[[names(DATA)[i]]] = dplyr::tbl(ms, names(DATA)[i])
+#  for (i in seq_along(DATA)) {
+#    res_db[[names(DATA)[i]]] <- dplyr::tbl(ms, names(DATA)[i])
 #  }
 #  
 #  # Your data is ready to use
@@ -87,26 +86,26 @@ as_tibble(data.frame(Sample = c("immunoseq1", "immunoseq2", "immunoseq3"), strin
 ## ----basic-data---------------------------------------------------------------
 top(immdata$data[[1]])
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  coding(immdata$data[[1]])
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  noncoding(immdata$data[[1]])
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  nrow(inframes(immdata$data[[1]]))
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  nrow(outofframes(immdata$data[[1]]))
 
 ## -----------------------------------------------------------------------------
-filter(immdata$data[[1]], V.name == 'TRBV10-1')
+filter(immdata$data[[1]], V.name == "TRBV10-1")
 
 ## -----------------------------------------------------------------------------
-ds = repSample(immdata$data, "downsample", 100)
+ds <- repSample(immdata$data, "downsample", 100)
 sapply(ds, nrow)
 
 ## -----------------------------------------------------------------------------
-ds = repSample(immdata$data, "sample", .n = 10)
+ds <- repSample(immdata$data, "sample", .n = 10)
 sapply(ds, nrow)
 
